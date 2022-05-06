dc487c3feb8b5f7e3937cddb8a3e9828a2230a03
Zugfuerer <101132940+Zugfuerer@users.noreply.github.com>
2022-05-06 13:02:00 +0200

    Add files via upload

---
+++ lang.php
@@ -0,0 +1,21 @@
+<?php
+ $lang=Array();
+ // 
+ $lang[0]['lname']='English';
+ $lang[1]['lname']='German';
+ $lang[2]['lname']='Chinese';
+ 
+ $lang[0]['Team']='team';
+ $lang[1]['Team']='Team';
+ $lang[2]['Team']='bigas';
+ 
+ $lang[0]['Impressum']='imprint';
+ $lang[1]['Impressum']='Impressum';
+ $lang[2]['Impressum']='imprimere';
+ 
+ 
+ 
+ 
+ 
+ 
+ ?>
